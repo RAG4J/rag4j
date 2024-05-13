a58b692b5c7b329fedfedb4a3fd325d633daec8c
Jettro Coenradie <jettro@coenradie.com>
2024-05-13 23:24:32 +0200

    Introduces new OpenAI model GPT-4o

--- src/main/java/org/rag4j/integrations/openai/OpenAIConstants.java
+++ src/main/java/org/rag4j/integrations/openai/OpenAIConstants.java
@@ -4,11 +4,12 @@ package org.rag4j.integrations.openai;
  * Constants for OpenAI engines. Contains constants for used models.
  */
 public interface OpenAIConstants {
+    String GPT4O = "gpt-4o";
     String GPT4 = "gpt-4";
-    String GPT4TURBO = "gpt-4-turbo-preview";
+    String GPT4TURBO = "gpt-4-turbo";
     String GPT35 = "gpt-3.5-turbo";
 
-    String DEFAULT_MODEL = GPT4;
+    String DEFAULT_MODEL = GPT4O;
 
     String ADA2 = "text-embedding-ada-002";
     String SMALL = "text-embedding-3-small";
